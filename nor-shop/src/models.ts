e9e41e37fb88ff74c2b1129a5d2c9c49f2627c46
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-11-22 08:30:25 +0100

    created constand and type models

---
+++ nor-shop/src/models.ts
@@ -0,0 +1,18 @@
+export interface Product {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    discountedPrice: number;
+    imageUrl: string;
+    rating: number;
+    tags : string[];
+    reviews: Review[];
+}
+
+export interface Review {
+    id: string;
+    username: string;
+    rating: number;
+    description: string;
+}
