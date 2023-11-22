89ab11c5974709562dbbfcea6d9c699ba5066b06
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-11-22 09:18:30 +0100

    set up react router

--- nor-shop/src/index.tsx
+++ nor-shop/src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <Route path="/" element={<Layout />}> */}
+      <Route index element={<App />} />
+      {/* <Route path="product/:id" element={<Product />} /> */}
+      {/* <Route path="*" element={<RouteNotFound />} />
+        </Route> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
