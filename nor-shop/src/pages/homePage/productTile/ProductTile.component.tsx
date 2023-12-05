a2751efec18adcb7a7f623bb0c4ad16cbd5c78b1
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-05 13:15:50 +0100

    new design on productTile

--- nor-shop/src/pages/homePage/productTile/ProductTile.component.tsx
+++ nor-shop/src/pages/homePage/productTile/ProductTile.component.tsx
@@ -11,38 +11,50 @@ export default function ProductTile({ product }: { product: Product }) {
     <Link to={`/product/${product.id}`}>
       <motion.div
         key={product.id}
-        whileHover={{ scale: 1.05 }}
-        transition={{ duration: 0.05 }}
-        className="rounded-md shadow bg-white p-3 flex flex-col justify-between gap-3 hover:cursor-pointer lg:w-[25vw] min-h-full lg:opacity-80 hover:lg:opacity-100 transition-all"
+        whileHover={{
+          scale: 1.05,
+          shadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+        }}
+        className={`rounded-md shadow bg-white text-primary flex flex-col lg:w-[25vw] justify-between relative ${
+          isSale && " border border-red-500"
+        }`}
       >
-        <div className="flex justify-between">
-          <h2 className="text-lg">{product.title}</h2>
-          {isSale && <span className="text-red-500 font-bold">SALE</span>}
-        </div>
-        <p className="line-clamp-2">{product.description}</p>
-        <div className="flex flex-col gap-1">
+        {isSale && (
+          <p className="text-red-500 absolute top-3 left-3 font-bold tracking-widest bg-white border border-red-500 rounded py-1 px-2">
+            SALE
+          </p>
+        )}
+
+        <div className="w-full h-60 border-b shadow-inner">
           <img
             src={product.imageUrl}
             alt={product.title}
-            className="w-full h-60 lg:h-80 object-cover"
+            className="h-full w-full object-cover rounded-t-md"
           />
-          <p>Click to view</p>
         </div>
-        <StarRatings
-          rating={product.rating}
-          numberOfStars={5}
-          starRatedColor="gold"
-          starDimension="20px"
-          starSpacing="1px"
-        ></StarRatings>
 
-        <div className="flex flex-row-reverse gap-1 justify-between items-end">
-          <p>{FormatPrice(product.price)}</p>
-          {isSale && (
-            <p className="text-green-500 text-sm">
-              Save {FormatPrice(product.price - product.discountedPrice)}
-            </p>
-          )}
+        <div className="flex flex-col justify-between gap-3 h-fit p-3 ">
+          <div className="flex justify-between items-baseline">
+            <h2 className="text-lg font-bold max-w-[65%] line-clamp-1">
+              {product.title}
+            </h2>
+            <StarRatings
+              rating={product.rating}
+              numberOfStars={5}
+              starRatedColor="gold"
+              starDimension="20px"
+              starSpacing="0px"
+            />
+          </div>
+
+          <div className="flex justify-between ">
+            {isSale && (
+              <p className="text-green-500 text-sm ">
+                Save {FormatPrice(product.price - product.discountedPrice)}
+              </p>
+            )}
+            <p>{FormatPrice(product.price)}</p>
+          </div>
         </div>
       </motion.div>
     </Link>
