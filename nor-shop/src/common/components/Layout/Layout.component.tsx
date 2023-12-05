243f94f7f7c1af2cc0708c96dc2b38a35781147e
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-05 11:34:28 +0100

    fixed footer location

--- nor-shop/src/common/components/Layout/Layout.component.tsx
+++ nor-shop/src/common/components/Layout/Layout.component.tsx
@@ -1,14 +1,15 @@
-import { Product } from "../../models/models";
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header.component";
 
 export default function Layout() {
   return (
-    <div className="bg-background">
+    <div className="bg-background ">
       <Header />
-      <Outlet />
-      {/* <Footer /> */}
-      "FOOTER"
+      <div className="pt-20 flex flex-col h-full justify-between min-h-[calc(100vh-5rem)] lg:min-h-[100vh] ">
+        <Outlet />
+        {/* <Footer /> */}
+        <div className="border border-blue-500">FOOTER</div>
+      </div>
     </div>
   );
 }
--- nor-shop/src/common/components/header/Header.component.tsx
+++ nor-shop/src/common/components/header/Header.component.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav.component";
 
 export default function Header() {
   return (
-    <header className="w-full bg-white rounded-md px-3 py-5 lg:px-0 shadow ">
+    <header className="w-full bg-white rounded-md px-3 py-5 lg:px-0 shadow-lg fixed z-50">
       <div className="flex justify-between lg:w-[90vw] 2xl:w-[1500px] lg:mx-auto">
         <div className="flex gap-4 items-center">
           <Logo height={30} width={30} />
