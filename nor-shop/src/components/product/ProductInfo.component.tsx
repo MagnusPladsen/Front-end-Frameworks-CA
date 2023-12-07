589df53306f1b3d92b6d7ff10a1edba6f096f013
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-07 09:23:00 +0100

    desing changes

--- nor-shop/src/components/product/ProductInfo.component.tsx
+++ nor-shop/src/components/product/ProductInfo.component.tsx
@@ -1,9 +1,11 @@
-import ProductStars from "./ProductStars.component";
-import FormatPrice from "../../common/formatters/FormatPrice.component";
+import FormatPrice from "../formatters/FormatPrice.component";
 import { Product } from "../../common/models/models";
-import { motion } from "framer-motion";
-import BuyButton from "../buttons/BuyButton.component";
+import BuyButton from "../buttons/DefaultButton.component";
 import SaleBadge from "../sale/SaleBadge.component";
+import ProductStars from "./ProductStars.component";
+import ProductTitle from "./ProductTitle.component";
+import ProductBody from "./ProductBody.component";
+import ProductImage from "./ProductImage.component";
 
 export default function ProductInfo({
   product,
@@ -22,48 +24,18 @@ export default function ProductInfo({
   return (
     <div
       key={product.id}
-      className={`text-primary flex flex-col lg:flex-row justify-between min-h-full relative items-end `}
+      className={`text-primary flex flex-col lg:flex-row justify-between w-full min-h-full relative items-end `}
     >
       <SaleBadge isSale={isSale} />
 
-      <div className="w-full h-60 lg:h-[600px] shadow-inner">
-        <img
-          src={product.imageUrl}
-          alt={product.title}
-          className="h-full w-full object-cover"
+      <ProductImage srcUrl={product.imageUrl} alt={product.title} fullSize />
+
+      <div className="w-full flex flex-col gap-3 h-full p-3 lg:p-6 lg:gap-5 pb-10 lg:max-w-md">
+        <ProductTitle
+          product={product}
+          onStarsClick={() => scrollTo(reviewsRef)}
         />
-      </div>
-
-      <div className="flex flex-col gap-3 h-fit p-3 lg:gap-5 pb-5">
-        <div className="flex justify-between items-baseline ">
-          <h1 className="text-lg font-bold">{product.title}</h1>
-          <div
-            className="transition-all hover:opacity-80 hover:cursor-pointer"
-            onClick={() => scrollTo(reviewsRef)}
-          >
-            <ProductStars
-              rating={product.rating}
-              reviewsAmount={product.reviews.length}
-            />
-          </div>
-        </div>
-
-        <p className="text-black">{product.description}</p>
-        <div className="flex justify-between flex-row-reverse"></div>
-        <div className="flex justify-between pt-0 items-end">
-          <div className="flex flex-col">
-            {isSale && (
-              <p className="text-green-500 text-sm ">
-                Save {FormatPrice(product.price - product.discountedPrice)}
-              </p>
-            )}
-            <p className={`font-bold text-xl ${isSale && "text-red-500 "}`}>
-              {FormatPrice(product.price)}
-            </p>
-          </div>
-
-          <BuyButton />
-        </div>
+        <ProductBody product={product} isSale={isSale} />
       </div>
     </div>
   );
--- nor-shop/src/components/productTile/ProductTile.component.tsx
+++ nor-shop/src/components/productTile/ProductTile.component.tsx
@@ -1,56 +1,42 @@
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
-import ProductStars from "../product/ProductStars.component";
-import FormatPrice from "../../common/formatters/FormatPrice.component";
 import { Product } from "../../common/models/models";
+import ProductImage from "../product/ProductImage.component";
+import ProductPrice from "../product/ProductPrice.component";
+import ProductStars from "../product/ProductStars.component";
 import SaleBadge from "../sale/SaleBadge.component";
 
 export default function ProductTile({ product }: { product: Product }) {
   const isSale = product.price !== product.discountedPrice;
 
   return (
-    <Link to={`/product/${product.id}`}>
-      <motion.div
-        key={product.id}
-        whileHover={{
-          scale: 1.05,
-          shadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
-        }}
-        className={`rounded-md shadow bg-white text-primary flex flex-col lg:w-[calc(90vw/3.1)] 2xl:w-80 justify-between relative ${
-          isSale && " border border-red-500"
-        }`}
-      >
-        <SaleBadge isSale={isSale} />
+    <motion.div
+      key={product.id}
+      className={`rounded-md shadow bg-white text-primary flex flex-col lg:w-[calc(90vw/3.1)] 2xl:w-80 justify-between relative ${
+        isSale && " border border-red-500"
+      }`}
+    >
+      <SaleBadge isSale={isSale} />
 
-        <div className="w-full h-60 border-b shadow-inner">
-          <img
-            src={product.imageUrl}
-            alt={product.title}
-            className="h-full w-full object-cover rounded-t-md"
+      <ProductImage srcUrl={product.imageUrl} alt={product.title} />
+
+      <div className="flex flex-col justify-between gap-3 h-fit p-3 ">
+        <div className="flex justify-between items-baseline">
+          <h2 className="text-lg font-bold max-w-[55%] line-clamp-1">
+            {product.title}
+          </h2>
+          <ProductStars
+            rating={product.rating}
+            reviewsAmount={product.reviews.length}
           />
         </div>
 
-        <div className="flex flex-col justify-between gap-3 h-fit p-3 ">
-          <div className="flex justify-between items-baseline">
-            <h2 className="text-lg font-bold max-w-[55%] line-clamp-1">
-              {product.title}
-            </h2>
-            <ProductStars
-              rating={product.rating}
-              reviewsAmount={product.reviews.length}
-            />
-          </div>
-
-          <div className="flex justify-between flex-row-reverse">
-            <p>{FormatPrice(product.price)}</p>
-            {isSale && (
-              <p className="text-green-500 text-sm ">
-                Save {FormatPrice(product.price - product.discountedPrice)}
-              </p>
-            )}
-          </div>
-        </div>
-      </motion.div>
-    </Link>
+        <ProductPrice
+          product={product}
+          isSale={isSale}
+          priceClassName="!text-base"
+          viewButton
+        />
+      </div>
+    </motion.div>
   );
 }
