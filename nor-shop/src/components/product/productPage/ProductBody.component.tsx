885d33a5b29a38e209b0dd74fffa728dfc58b6f4
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-07 10:32:20 +0100

    added tags

--- nor-shop/src/components/product/productPage/ProductBody.component.tsx
+++ nor-shop/src/components/product/productPage/ProductBody.component.tsx
@@ -11,6 +11,14 @@ export default function ProductBody({
   return (
     <>
       <p className="text-black pb-5">{product.description}</p>
+      <p className="">Tags: {" "}
+        {product.tags.map((tag, index) => (
+          <span key={tag} className="text-primary mr-1">
+            {tag}
+            {index !== product.tags.length - 1 && ","}
+          </span>
+        ))}
+      </p>
       <ProductPrice product={product} isSale={isSale} buyButton />
     </>
   );
