99d7b5c0067752b07fa4f5d0234fa07e8ac14aa5
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-20 11:27:34 +0100

    small change to footer design

--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="bg-white mt-20 w-full px-3 py-10 lg:px-0 shadow-lg z-50">
       <div className="flex lg:w-[90vw] 2xl:w-[1500px] flex-col lg:flex-row lg:mx-auto lg:items-center justify-between gap-10 lg:gap-5">
-        <div className="flex gap-5 items-center justify-between">
+        <div className="flex gap-5 items-center ">
           <Link to="/">
             <motion.div
               whileHover={{ scale: 1.1, opacity: 0.6 }}
@@ -17,7 +17,7 @@ export default function Footer() {
           </Link>
           <p className="text-xs text-primary">- We got what you need...</p>
         </div>
-        <div className="flex flex-col lg:flex-row justify-end gap-2 lg:gap-5">
+        <div className="flex flex-col lg:flex-row justify-end gap-2 lg:gap-20">
           <p className="text-xs text-primary">Phone: +47 98 76 54 32</p>
           <p className="text-xs text-primary">Email: contact@norshop.no</p>
         </div>
