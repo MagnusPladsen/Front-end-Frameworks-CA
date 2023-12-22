158fed8632aa69ef2277e3fbde30e7955128a406
Magnus Pladsen <magnus_pladsen@hotmail.com>
2023-12-22 19:48:59 +0100

    WIP working on tag sorting

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,9 +4,15 @@ import { Product } from "../../models/models";
 import ProductsSearch from "../../components/home/ProductsSearch.component";
 import PageStateHandler from "../../components/pageStateHandler/PageStateHandler.component";
 import ProductTile from "../../components/tile/ProductTile.component";
+import { useLocation } from "react-router-dom";
+import H1 from "../../components/text/H1.component";
 
 export default function HomePage() {
   const { products, isLoading, error } = useProductsContext();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const tagQuery = queryParams.get("tag");
 
   const [filteredProducts, setFilteredProducts] = useState<Product[]>(products);
 
@@ -18,18 +24,32 @@ export default function HomePage() {
     >
       <div className="p-3 py-5 lg:py-10 lg:px-10 w-full">
         <div className=" lg:w-[90vw] 2xl:w-[1500px] lg:mx-auto flex flex-col gap-5 lg:gap-10">
-          <ProductsSearch
-            setFilteredProducts={setFilteredProducts}
-            products={products}
-          />
+          {!!!tagQuery ? (
+            <ProductsSearch
+              setFilteredProducts={setFilteredProducts}
+              products={products}
+            />
+          ) : (
+              <H1>{tagQuery}<H1 />
+          )}
           <div className="flex flex-col lg:flex-row lg:flex-wrap gap-3 justify-center">
-            {filteredProducts.length > 0 ? (
-              filteredProducts.map((product: Product) => (
-                <ProductTile product={product} key={product.id} />
-              ))
-            ) : (
-              <p>No products matching this search were found...</p>
-            )}
+            {!!!tagQuery
+              ? filteredProducts.map((product: Product) => {
+                  if (filteredProducts.length > 0) {
+                    return <ProductTile product={product} key={product.id} />;
+                  }
+                  return (
+                    <p key={product.id}>
+                      No products matching this search were found...
+                    </p>
+                  );
+                })
+              : products.map((product: Product) => {
+                  if (product.tags.includes(tagQuery)) {
+                    return <ProductTile product={product} key={product.id} />;
+                  }
+                  return null;
+                })}
           </div>
         </div>
       </div>
